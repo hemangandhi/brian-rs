8a8624fe4aaf4f28a120f96f9dc0e40f19c9af98
hemangandhi <hemangandhi@gmail.com>
2021-02-14 00:36:55 -0500

    Corrected neuron abstractions

--- src/lib.rs
+++ src/lib.rs
@@ -8,16 +8,16 @@ mod tests {
     use dim::si;
     use std::vec;
 
-    use super::spike_generators::{SpikeAtTimes, SpikeGenerator};
+    use super::spike_generators::{discrete::SpikeAtTimes, SpikeGenerator, InputSpikeGenerator};
 
     #[test]
     fn spike_generator_at_times() {
         let times = vec![1. * si::S, 2. * si::S];
-        let mut spiker = SpikeAtTimes::new(times.clone(), 0.1 * si::S);
+        let mut spiker = SpikeAtTimes::new(times.clone(), 0.1 * si::S, 0.3 * si::A);
 
         for i in 0..=30 {
             assert_eq!(spiker.did_spike(), i == 5 || i == 10);
-            spiker.try_advance(0.2 * si::S);
+            spiker.advance(0.2 * si::S);
         }
     }
 }
--- src/spike_generators.rs
+++ src/spike_generators.rs
@@ -1,69 +1,147 @@
-extern crate dimensioned as dim;
-
-use dim::dimensions;
-use dim::si;
-
-pub trait SpikeGenerator {
+pub trait SpikeGenerator<I> {
     fn did_spike(&self) -> bool;
-    fn try_advance(&mut self, dt: si::Second<f64>) -> bool;
+    fn get_current(&self) -> I;
+}
 
-    fn advance_and_check(&mut self, dt: si::Second<f64>) -> Option<bool> {
-        if !self.try_advance(dt) {
-            Option::None
-        } else {
-            Option::Some(self.did_spike())
+pub trait InnerSpikeGenerator<I, T>: SpikeGenerator<I> {
+    fn handle_input(&mut self, input: I, dt: T);
+}
+
+pub trait InputSpikeGenerator<I, T>: SpikeGenerator<I> {
+    fn advance(&mut self, dt: T);
+}
+
+pub mod discrete {
+    extern crate dimensioned as dim;
+
+    use dim::dimensions;
+    use dim::si;
+
+    #[derive(Debug)]
+    pub struct SpikeAtTimes<T, I> {
+        times: Vec<T>,
+        time: T,
+        error_tolerance: T,
+        idx: usize,
+        spike_current: I,
+    }
+
+    impl<T: From<si::Second<f64>>, I> SpikeAtTimes<T, I> {
+        pub fn new(times: Vec<T>, tolerance: T, spike_current: I) -> SpikeAtTimes<T, I> {
+            SpikeAtTimes {
+                times: times,
+                time: (0.0 * si::S).into(),
+                error_tolerance: tolerance,
+                idx: 0,
+                spike_current: spike_current,
+            }
+        }
+    }
+
+    impl<T, I> super::SpikeGenerator<I> for SpikeAtTimes<T, I>
+    where
+        // TODO: alias this as a trait?
+        T: From<si::Second<f64>>
+            + Copy
+            + PartialOrd<T>
+            + std::ops::AddAssign
+            + std::ops::Sub<Output = T>
+            + std::ops::Neg<Output = T>,
+        I: From<si::Ampere<f64>> + Copy,
+    {
+        fn did_spike(&self) -> bool {
+            let idx = if self.idx >= self.times.len() {
+                self.times.len() - 1
+            } else {
+                self.idx
+            };
+            let time_diff = self.times[idx] - self.time;
+            return -self.error_tolerance < time_diff && time_diff < self.error_tolerance;
+        }
+
+        fn get_current(&self) -> I {
+            if self.did_spike() {
+                self.spike_current.into()
+            } else {
+                (0.0 * si::A).into()
+            }
+        }
+    }
+
+    impl<T, I> super::InputSpikeGenerator<I, T> for SpikeAtTimes<T, I>
+    where
+        // TODO: alias this as a trait?
+        T: From<si::Second<f64>>
+            + Copy
+            + PartialOrd<T>
+            + std::ops::AddAssign
+            + std::ops::Sub<Output = T>
+            + std::ops::Neg<Output = T>,
+        I: From<si::Ampere<f64>> + Copy,
+    {
+        fn advance(&mut self, dt: T) {
+            self.time += dt.into();
+            while self.idx < self.times.len() && self.times[self.idx] < self.time {
+                self.idx += 1;
+            }
         }
     }
 }
 
-pub trait SpikeGeneratorWithInput<Input: dimensions::Current>: SpikeGenerator {
-    fn handle_input(&mut self, input: Input, dt: impl dimensions::Time);
-}
+pub mod continuous {
+    extern crate dimensioned as dim;
 
-#[derive(Debug)]
-pub struct SpikeAtTimes<T> {
-    times: Vec<T>,
-    time: T,
-    error_tolerance: T,
-    idx: usize,
-}
+    use dim::dimensions;
+    use dim::si;
 
-impl<T: From<si::Second<f64>>> SpikeAtTimes<T> {
-    pub fn new(times: Vec<T>, tolerance: T) -> SpikeAtTimes<T> {
-        SpikeAtTimes {
-            times: times,
-            time: (0.0 * si::S).into(),
-            error_tolerance: tolerance,
-            idx: 0,
-        }
-    }
-}
-
-impl<T> SpikeGenerator for SpikeAtTimes<T>
-where
-    // TODO: alias this as a trait?
-    T: From<si::Second<f64>>
-        + Copy
-        + PartialOrd<T>
-        + std::ops::AddAssign
-        + std::ops::Sub<Output = T>
-        + std::ops::Neg<Output = T>,
-{
-    fn did_spike(&self) -> bool {
-        let idx = if self.idx >= self.times.len() {
-            self.times.len() - 1
-        } else {
-            self.idx
-        };
-        let time_diff = self.times[idx] - self.time;
-        return -self.error_tolerance < time_diff && time_diff < self.error_tolerance;
+    #[derive(Debug)]
+    pub struct ContinuousEquivalent<D, T, I> {
+        last_spike: T,
+        spike_time_scale: f64,
+        spike_decay_scale: f64,
+        discrete_neuron: D,
+        spike_current: I,
     }
 
-    fn try_advance(&mut self, dt: si::Second<f64>) -> bool {
-        self.time += dt.into();
-        while self.idx < self.times.len() && self.times[self.idx] < self.time {
-            self.idx += 1;
+    impl<T: From<si::Second<f64>>, D, I: From<si::Ampere<f64>>> ContinuousEquivalent<D, T, I> {
+        pub fn new(discrete_neuron: D, time_scale: f64, decay_scale: f64) -> Self {
+            ContinuousEquivalent {
+                last_spike: (0.0 * si::S).into(),
+                spike_time_scale: time_scale,
+                spike_decay_scale: decay_scale,
+                discrete_neuron: discrete_neuron,
+                spike_current: (0.0 * si::A).into(),
+            }
         }
-        true
+    }
+
+    impl<D, T, I> super::SpikeGenerator<I> for ContinuousEquivalent<D, T, I>
+    where
+        D: super::SpikeGenerator<I>,
+        T: Into<si::Second<f64>> + Copy,
+        I: From<si::Ampere<f64>> + Into<si::Ampere<f64>> + Copy,
+    {
+        fn did_spike(&self) -> bool {
+            self.discrete_neuron.did_spike()
+        }
+        fn get_current(&self) -> I {
+            if self.did_spike() {
+                self.discrete_neuron.get_current()
+            } else {
+                ((self.last_spike.into() / si::S * self.spike_time_scale).exp()
+                    * self.spike_decay_scale
+                    * self.spike_current.into())
+                .into()
+            }
+        }
+    }
+
+    impl<D, T, I> super::InputSpikeGenerator<I, T> for ContinuousEquivalent<D, T, I>
+    where
+        D: super::InputSpikeGenerator<I, T>,
+        T: From<si::Second<f64>> + Into<si::Second<f64>> + Copy,
+        I: From<si::Ampere<f64>> + Into<si::Ampere<f64>> + Copy,
+    {
+        fn advance(&mut self, dt: T) {}
     }
 }
